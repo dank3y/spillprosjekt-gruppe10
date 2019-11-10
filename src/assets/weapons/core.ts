835458b19566aded51a4835fc0c86220bd51a501
Andreas Ipsen <andr940f@gmail.com>
2019-11-10 14:48:29 +0100

    starten på våpen

---
+++ src/assets/weapons/core.ts
@@ -0,0 +1,48 @@
+import { Sprite } from "../core";
+
+
+
+// ROF er Rate Of Fire, med enhet Rounds/min
+export class Weapon extends Sprite {
+  public leftInMag: number;
+  public lastBullet: number;
+
+  constructor(
+    x: number,
+    y: number,
+    _sprite: string,
+    public magSize: number,
+    public reloadTime: number,
+    public ROF: number,
+    width: number,
+    height: number,
+  ) {
+    super(x, y, _sprite, width, height);
+
+    this.leftInMag = this.magSize;
+  }
+  shoot(): void {}
+}
+
+// "g" tilsier hvor mye prosjektilet skal falle,
+// vel tilsier hastighet
+export class Projectile extends Sprite {
+  constructor(
+    x: number,
+    y: number,
+    _sprite: string,
+    width: number,
+    height: number,
+    public angle: number,
+    public g: number,
+    public vel: number,
+  ) {
+    super(x, y, _sprite, width, height,);
+  }
+  
+  update(){
+    this.x += Math.cos(this.angle) * this.vel;
+    this.y += Math.sin(this.angle) * this.vel;
+  }
+
+}
---
+++ src/assets/weapons/pistol/pistol.ts
@@ -0,0 +1,19 @@
+import { Weapon } from "../core";
+
+const sprite = require('./sprite.png')
+
+
+const magSize = 15;
+const reloadTime = 1.5;
+const ROF = 1100;
+
+export class Pistol extends Weapon {
+  constructor(
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ){
+    super(x, y, sprite, magSize, reloadTime, ROF, width, height);
+  }
+}
