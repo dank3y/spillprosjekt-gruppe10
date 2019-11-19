7496cc250c2b4ec16b6939c550cefc7214bda237
Andreas Ipsen <andreaip@stud.ntnu.no>
2019-11-19 19:49:16 +0100

    =la til progress bar på reload

--- src/engine/UI/ammo-counter.ts
+++ src/engine/UI/ammo-counter.ts
@@ -26,7 +26,7 @@ export class AmmoCounter extends UIElement{
         ctx.save();
         ctx.font = '26px gamefont';
         let text = 'AMMO';
-        ctx.fillText(text, 0.5 * (this.width - ctx.measureText(text).width), this.height - 40);
+        ctx.fillText(text, 0.5 * (this.width - ctx.measureText(text).width), this.height - 60);
         ctx.restore();
         ctx.closePath();
     }
@@ -36,9 +36,24 @@ export class AmmoCounter extends UIElement{
         ctx.save();
         ctx.font = '30px gamefont';
         let text = `${this.target.weapon!.leftInMag}/${this.target.weapon!.magSize}`;
-        ctx.fillText(text, 0.5 * (this.width - ctx.measureText(text).width), this.height - 10)
+        ctx.fillText(text, 0.5 * (this.width - ctx.measureText(text).width), this.height - 30)
         ctx.restore();
         ctx.closePath();
+    }
+
+    drawReloadProgress(ctx: CanvasRenderingContext2D){
+        if (this.target.weapon.reloading){
+            let factor = Number(((Date.now() - this.target.weapon.reloadStart) / (this.target.weapon.reloadTime * 1000)).toFixed(2));
+            ctx.beginPath();
+            ctx.save();
+            ctx.fillStyle = '#000';
+            ctx.fillRect(0.5 * (this.width - factor * 0.8 * this.width), this.height - 20, factor * 0.8 * this.width, 15);
+            ctx.fillStyle = '#f00';
+            let text = 'RELOADING';
+            ctx.fillText(text, 0.5 * (this.width - ctx.measureText(text).width), this.height - 10);
+            ctx.restore();
+            ctx.closePath();
+        }
     }
 
     _draw(ctx: CanvasRenderingContext2D): void {
@@ -46,11 +61,12 @@ export class AmmoCounter extends UIElement{
         this.drawBox(ctx);
         this.drawAmmo(ctx);
         this.drawDesc(ctx);
+        this.drawReloadProgress(ctx);
         this.drawOutline(ctx);
     }
 
     constructor(public target: InstanceType<typeof NPC>){
-        super(120, 70, 'end', 'end');
+        super(120, 100, 'end', 'end');
         super.draw = this._draw;
         
     }
